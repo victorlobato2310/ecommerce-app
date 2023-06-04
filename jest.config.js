2073f2939e061158d6da0ef8b3ce273421a4a005
Victor Lobato <victorlobato2310@gmail.com>
2023-06-04 15:10:03 -0300

    build: add collectCoverage and collectCoverageFrom

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,17 @@
 module.exports = {
   preset: 'jest-preset-angular',
   roots: ['<rootDir>/src'],
-  testMatch: ['**/tests/**/*.+(ts|js)', '**/?(*.)+(spec|test).+(ts|js)'],
   moduleNameMapper: {
     '^@src(.*)$': '<rootDir>/src$1',
     '^@app/components(.*)$': '<rootDir>/src/app/components$1',
     '^@app/services(.*)$': '<rootDir>/src/app/services$1',
   },
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  collectCoverageFrom: [
+    "src/app/**/*.ts",
+    "!src/app/**/*.module.ts"
+  ],
   moduleDirectories: ['node_modules', 'src'],
   setupFilesAfterEnv: ['<rootDir>/setupJest.ts'],
 };
