24ceed66a3ea84c28d14420524d068b80b46c770
Victor Lobato <victorlobato2310@gmail.com>
2023-06-04 15:53:02 -0300

    test: add header suite cases

--- src/app/tests/components/header/header.component.spec.ts
+++ src/app/tests/components/header/header.component.spec.ts
@@ -1,17 +1,70 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HeaderComponent } from '@app/components/header/header.component';
+import { LocalStorageService } from '@src/app/services/local-storage.service';
 import "@src/test-setup";
 
+const LocalStorageServiceMock = {
+  set: jest.fn(),
+  get: jest.fn(),
+  delete: jest.fn()
+}
+
 describe('HeaderComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [HeaderComponent]
-  }));
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
+  let mockItem =  {
+    name: 'Caneca de cerâmica rústica',
+    description: "lorem lorem",
+    image_url: 'https://storage.googleapis.com/xesque-dev/challenge-images/caneca-06.jpg',
+    category: 'mugs'
+  }
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [HeaderComponent],
+      imports: [RouterTestingModule.withRoutes([]),],
+      providers: [{ provide: LocalStorageService, value: LocalStorageServiceMock }]
+    }).compileComponents();
+    jest.useFakeTimers();
+    fixture = TestBed.createComponent(HeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(HeaderComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
+  });
+
+  it('should call ngOnInit() method and return no items from localStorage', () => {
+    jest.spyOn(component, 'ngOnInit');
+    component.ngOnInit()
+    expect(component.ngOnInit).toHaveBeenCalled();
+    jest.advanceTimersByTime(1000);
+    jest.useRealTimers();
+    expect(component.shoppingCartItemsCount).toBe(0);
+    expect(component.selectedItems.length).toBe(0);
+  });
+
+  it('should call ngOnInit() method and return 1 item from localStorage', () => {
+    let localStorageService = TestBed.inject(LocalStorageService);
+    jest.spyOn(localStorageService, 'set');
+    localStorageService.set("shoppingCart", mockItem);
+    jest.spyOn(component, 'ngOnInit');
+    component.ngOnInit()
+    expect(component.ngOnInit).toHaveBeenCalled();
+    jest.advanceTimersByTime(1000);
+    jest.useRealTimers();
+    expect(component.shoppingCartItemsCount).toEqual(1);
+    expect(component.selectedItems.length).toBeGreaterThan(0);
+    expect(localStorageService.set).toHaveBeenCalledWith("shoppingCart", mockItem);
+    expect(component.selectedItems[0]).toEqual(mockItem);
+    expect(component.selectedItems[0].category).toEqual("mugs");
+  });
+
+  it('should call ngOnDestroy() method', () => {
+    jest.spyOn(component, 'ngOnDestroy');
+    component.ngOnDestroy()
+    expect(component.ngOnDestroy).toHaveBeenCalled();
   });
 });
